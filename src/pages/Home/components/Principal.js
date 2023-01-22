6cb7b85ccf79d515633133ed31fd2f76be4f34cf
Jeniffer Yaguana <jyaguana3697@uta.edu.ec>
2023-01-22 01:40:59 -0500

    DBS-31 Desarrollar la interfaz del inicio de la aplicación web

---
+++ src/pages/Home/components/Principal.js
@@ -0,0 +1,11 @@
+import React  from 'react';
+
+
+class Principal extends React.Component {
+    render() {
+        return (
+    <p> SOY EL INICIO</p>
+        );
+    }
+}
+export default Principal;
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,13 +1,157 @@
+import { Layout, Space, Menu } from "antd";
+import "../Home/home.css";
+import logo from "../../assets/images/cliente.png";
+import { HomeFilled, ToolFilled } from "@ant-design/icons";
+import { RiLogoutBoxRFill } from "react-icons/ri";
 import React from "react";
-import { Button } from 'antd';
+import Principal from "./components/Principal";
+import Configuracion from "./components/Configuracion";
+
+const { Header, Footer, Sider, Content } = Layout;
+const headerStyle = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 50,
+  lineHeight: "64px",
+  backgroundColor: "#7dbcea",
+};
+const contentStyle = {
+  textAlign: "center",
+  minHeight: 120,
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#108ee9",
+};
+const siderStyle = {
+  textAlign: "center",
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#3ba0e9",
+};
+const footerStyle = {
+  textAlign: "center",
+  color: "#fff",
+  backgroundColor: "#7dbcea",
+};
 
 class Home extends React.Component {
-    render() {
-        return (
-            <Button className='Button2' type="submit" shape="round" size="large" style={{ backgroundColor: '#1677ff', width: '250px', color: '#fff'}}>
-            Home
-          </Button>
-        );
+  state = {
+    selectedItem: (
+      <p>
+        <HomeFilled /> Inicio
+      </p>
+    ),
+    currentContent: <Principal />,
+  };
+
+  handleMenuClick = (e) => {
+    let menu = e.item.props.children[0][1].props.children;
+
+    switch (menu) {
+      case " Inicio":
+        this.setState({
+          selectedItem: e.item.props.children,
+          currentContent: <Principal />,
+        });
+        break;
+
+      case " Configuración":
+        this.setState({
+          selectedItem: e.item.props.children,
+          currentContent: <Configuracion />,
+        });
+        break;
+
+      default:
+        this.setState({
+          selectedItem: e.item.props.children,
+          currentContent: <Principal />,
+        });
     }
+  };
+
+  render() {
+    return (
+      <Space
+        direction="vertical"
+        style={{
+          width: "100%",
+        }}
+        size={[0, 48]}
+      >
+        <Layout>
+          <Sider
+            style={{ siderStyle, backgroundColor: "#000080", height: "100vh" }}
+          >
+            <div id="user-info">
+              <img src={logo} id="user-image" alt="sinloog" />
+              <span id="user-name" style={{ fontWeight: "650" }}>
+                Jeniffer Yaguana
+              </span>
+            </div>
+            <hr style={{ width: "185px", color: "#ffff" }} />
+
+            <Menu
+              style={{
+                backgroundColor: "#4169E1",
+                fontWeight: "800",
+                color: "#ffff",
+              }}
+              mode="inline"
+              onClick={this.handleMenuClick}
+              defaultSelectedKeys={["1"]}
+              items={[
+                {
+                  key: "1",
+                  icon: <HomeFilled />,
+                  label: " Inicio",
+                },
+              ]}
+            />
+            <div style={{ marginTop: "428px" }}>
+              <hr
+                style={{ width: "185px", marginBottom: "15px", color: "#ffff" }}
+              />
+              <Menu
+                style={{
+                  backgroundColor: "#4169E1",
+                  fontWeight: "800",
+                  color: "#ffff",
+                }}
+                mode="inline"
+                onClick={this.handleMenuClick}
+                items={[
+                  {
+                    key: "1",
+                    icon: <ToolFilled />,
+                    label: " Configuración",
+                  },
+                  {
+                    key: "2",
+                    icon: <RiLogoutBoxRFill />,
+                    label: " Cerrar Sesión",
+                  },
+                ]}
+              />
+            </div>
+          </Sider>
+          <Layout>
+            <Header
+              style={{ headerStyle, background: "#000080", color: "#ffff" }}
+            >
+              {this.state.selectedItem}
+            </Header>
+            <Content style={{ contentStyle, backgroundColor: "#D4D4D4" }}>
+              {this.state.currentContent}
+            </Content>
+            <Footer style={{ footerStyle, backgroundColor: "#000080" }}>
+              <p className="copyright"> &copy; 2023 CDG SOLUTIONS </p>
+            </Footer>
+          </Layout>
+        </Layout>
+      </Space>
+    );
+  }
 }
 export default Home;
